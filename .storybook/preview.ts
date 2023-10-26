594a2fe55cb9a9feb141bf7373a4d4de48df2b43
Marcelo <marcelo_oliscott@hotmail.com>
2023-10-26 15:11:05 -0300

    feat(storybook): storybook theme addon added and test acessibility a11y added
    I added the themes addon to the storybook and added the addon to test accessibility to the storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,13 @@
 import type { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
 
 const preview: Preview = {
   parameters: {
     layout: 'centered',
     actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: themes.dark,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
