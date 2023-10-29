931b061ebad92e4344f0a7d83e5be2ff8e0dfa91
Marcelo <marcelo_oliscott@hotmail.com>
2023-10-29 19:15:19 -0300

    chore(unit tests): add testing library
    - lib testing library added
    - configure testing library with storybook
    
    closes #11

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
     '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
